eaa185643c048681188c41cb5b57f3fda0badb3f
Chilex23 <onumaegbuchilex@gmail.com>
2022-08-15 15:23:21 +0100

    Heroku db

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://nnwsixatdsfgfv:df154d2383570e2d8eab0ef8a3f1edb7f34290e6bdbd8f72d97d759ebf0e875a@ec2-44-209-186-51.compute-1.amazonaws.com:5432/d2h2dl7orvp1o0'
+SQLALCHEMY_DATABASE_URI = 'postgresql://nnwsixatdsfgfv:df154d2383570e2d8eab0ef8a3f1edb7f34290e6bdbd8f72d97d759ebf0e875a@ec2-44-209-186-51.compute-1.amazonaws.com:5432/d2h2dl7orvp1o0'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ENGINE_OPTIONS = {
     "max_overflow": 15,
